84df45d916812fb5153fe25afed52600ec63e053
Md. Mehedi Hasan Porosh <porosh.diu@gmail.com>
2023-08-15 18:54:50 +0600

    signup page done

---
+++ JS/signup.js
@@ -0,0 +1,9 @@
+document.querySelector('.language').addEventListener('click',() => {
+ const classes = document.querySelector(".language ul").classList;
+
+  if(classes[0] === "show-language"){
+    classes.remove("show-language");
+  }else{
+    classes.add("show-language");
+  }
+})
